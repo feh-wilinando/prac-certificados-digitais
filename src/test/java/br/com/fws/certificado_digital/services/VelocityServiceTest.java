d66dcbc45291a31f1d201ae46ab44e13c6c3790d
Fernando Furtado <feh.wilinando@gmail.com>
2016-11-25 18:46:43 -0200

    Melhorado o Test para VelocityServices

--- src/test/java/br/com/fws/certificado_digital/services/VelocityServiceTest.java
+++ src/test/java/br/com/fws/certificado_digital/services/VelocityServiceTest.java
@@ -12,9 +12,8 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import br.com.caelum.stella.type.Estado;
-import br.com.fws.certificado_digital.models.Address;
-import br.com.fws.certificado_digital.models.Customer;
-import br.com.fws.certificado_digital.models.users.User;
+import br.com.fws.certificado_digital.models.customer.Address;
+import br.com.fws.certificado_digital.models.customer.Customer;
 
 public class VelocityServiceTest {
 
@@ -30,10 +29,26 @@ public class VelocityServiceTest {
 		velocityEngine.setProperty(RuntimeConstants.FILE_RESOURCE_LOADER_PATH, templateURL.getPath());
 		velocityEngine.init();
 		
-		Address address = new Address("Rua da Empresa", 1000, "18º andar", "São Paulo", Estado.SP, "11111-111");		
-		User user = new User("contato@empresa.com.br", "123");
+		Address address = new Address();//("Rua da Empresa", 1000, "18º andar", "São Paulo", Estado.SP, "11111-111");
+		address.setStreet("Rua da Empresa");
+		address.setNumber(1000);
+		address.setComplement("18º andar");
+		address.setCity("São Paulo");
+		address.setState(Estado.SP);
+		address.setZipCode("11111-111");
 		
-		customer = new Customer(1L, "Nome da Empresa", "11.111.111/1111-11", "", "1.111.111-1", address, user, "");
+		customer = new Customer(); 
+		customer.setId(1L);
+		customer.setCompanyName("Nome da Empresa");
+		customer.setCompanyRegistration("11.111.111/1111-11");
+		customer.setStateRegistration("2.222.222-2");
+		customer.setMunicipalRegistration("1.111.111-1");
+		customer.setAddress(address);
+		customer.setContact("Contato da Empresa");
+		customer.setEmail("email@empresa.com.br");		
+		customer.setTelephoneNumber("11 2323-2323");
+		customer.setCellPhoneNumber("11 9 9999-9999");
+		
 	}
 	
 	@Test
@@ -44,7 +59,7 @@ public class VelocityServiceTest {
 		VelocityService velocityService = new VelocityService(velocityEngine);
 		
 		String content = velocityService
-							.loadTemplate("email.vm")
+							.loadTemplate("customer.vm")
 							.getContent();
 		
 		assertThat(content, containsString("<strong>Empresa:</strong>"));
@@ -54,6 +69,7 @@ public class VelocityServiceTest {
 	}
 	
 	@Test
+	@SuppressWarnings("unchecked")
 	public void shouldFillOutTheTemplateEmailWithACustomer(){
 		VelocityService velocityService = new VelocityService(velocityEngine);
 		
@@ -61,14 +77,26 @@ public class VelocityServiceTest {
 		
 		
 		String content = velocityService
-							.loadTemplate("email.vm")
+							.loadTemplate("customer.vm")
 								.set("customer", customer)
 								.getContent();
+		
 		
 		assertThat(content, allOf(		containsString(customer.getCompanyName()),
 										containsString(customer.getCompanyRegistration()),
 										containsString(customer.getMunicipalRegistration()),
-										containsString(customer.getUser().getEmail())
+										containsString(customer.getStateRegistration()),
+										containsString(customer.getContact()),
+										containsString(customer.getEmail()),
+										containsString(customer.getTelephoneNumber()),
+										containsString(customer.getCellPhoneNumber()),
+										containsString(customer.getAddress().getStreet()),
+										containsString(customer.getAddress().getNumber().toString()),										
+										containsString(customer.getAddress().getComplement()),										
+										containsString(customer.getAddress().getZipCode()),										
+										containsString(customer.getAddress().getCity()),										
+										containsString(customer.getAddress().getState().name())
+										
 										
 									)
 				);
